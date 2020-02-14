8c9729decb1dd72cedd06f4b3d884d6b192435cc
Christopher Stock <christopher.stock@mayflower.de>
2020-02-14 16:08:35 +0100

    Moved main class to folder 'src'

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,16 @@
 ﻿
 using System;
 
-namespace DotNetPrimer
+namespace de.mayflower.dotnetprimer
 {
     public class Program
     {
         public static void Main( string[] args )
         {
             Console.WriteLine( "Hello World!" );
+
+            Console.WriteLine( "Hello .NET and C# World!" );
+
             Console.WriteLine( "Hello .NET and C# World!" );
         }
     }
