cd3f0f995923a7fda5fb8211919e0df6c4350839
Christopher Stock <christopher.stock@mayflower.de>
2020-02-14 15:46:58 +0100

    Added initial 'Hello World'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetPrimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
