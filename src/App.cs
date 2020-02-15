daad223b086624df3c3c9749c794ece0ea245f22
Christopher Stock <christopher.stock@mayflower.de>
2020-02-15 19:39:27 +0100

    Added some workshop logos. Renamed namespace to "DotNetPrimer".

---
+++ src/App.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace DotNetPrimer
+{
+    public abstract class App
+    {
+        public static void Main( string[] args )
+        {
+            Console.WriteLine( "Welcome to the DotNetPrimer App" );
+            
+            
+            
+            
+        }
+    }
+}
